1bd763d6811fe1a5bf5b3a2543ec03b38f909662
pierresy <pierre-henri.symoneaux@alcatel-lucent.com>
2015-11-09 20:35:06 +0100

    Updated documentation

--- implementors/collections/string/trait.ToString.js
+++ implementors/collections/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['prettytable'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a> for <a class='struct' href='prettytable/cell/struct.Cell.html' title='prettytable::cell::Cell'>Cell</a>",];
+implementors['prettytable'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a> for <a class='struct' href='prettytable/cell/struct.Cell.html' title='prettytable::cell::Cell'>Cell</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['term'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='term/enum.Attr.html' title='term::Attr'>Attr</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
