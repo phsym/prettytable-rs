aac360ceac297ed09e1f036381a9ebe9e4ab709d
pierresy <phsym@msn.com>
2016-01-03 12:02:10 +0100

    Added an example file using table slicing for #10

---
+++ examples/slices.rs
@@ -0,0 +1,28 @@
+#[macro_use] extern crate prettytable;
+
+use prettytable::Slice;
+
+fn main() {
+    let mut table = table![[0, 0, 0], [1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]];
+    table.set_titles(row![cell!("t1"), cell!("t2"), cell!("t3")]);
+
+    let slice = table.slice(..);
+    let slice = slice.slice(2..);
+    let slice = slice.slice(..3);
+
+    /*
+        Will print
+        +----+----+----+
+        | t1 | t2 | t3 |
+        +====+====+====+
+        | 2  | 2  | 2  |
+        +----+----+----+
+        | 3  | 3  | 3  |
+        +----+----+----+
+    */
+    slice.printstd();
+
+    // This is equivalent to
+    let slice = table.slice(2..3);
+    slice.printstd();
+}
