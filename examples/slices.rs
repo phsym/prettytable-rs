b7c143a0b4288eae0605703ecf435baa12cfcf9b
pierresy <phsym@msn.com>
2016-01-03 14:55:32 +0100

    Fixed slice example

--- examples/slices.rs
+++ examples/slices.rs
@@ -4,7 +4,7 @@ use prettytable::Slice;
 
 fn main() {
     let mut table = table![[0, 0, 0], [1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]];
-    table.set_titles(row![cell!("t1"), cell!("t2"), cell!("t3")]);
+    table.set_titles(row!["t1", "t2", "t3"]);
 
     let slice = table.slice(..);
     let slice = slice.slice(2..);
@@ -23,6 +23,6 @@ fn main() {
     slice.printstd();
 
     // This is equivalent to
-    let slice = table.slice(2..3);
+    let slice = table.slice(2..5);
     slice.printstd();
 }
