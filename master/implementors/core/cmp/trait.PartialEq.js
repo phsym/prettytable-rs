2d9fb6e54129f6c4775d2c51412ca0021cc87868
travis-ci <travis@travis-ci.org>
2016-01-13 15:19:01 +0000

    Updated documentation

---
+++ master/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['term'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='term/terminfo/parm/enum.Error.html' title='term::terminfo::parm::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='term/terminfo/enum.Error.html' title='term::terminfo::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='term/enum.Attr.html' title='term::Attr'>Attr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='term/enum.Error.html' title='term::Error'>Error</a>",];implementors['prettytable'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='prettytable/format/enum.Align.html' title='prettytable::format::Align'>Align</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
