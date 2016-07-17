a172ffbc5e68ab22ac503c5e8ed9d353893df8bf
travis-ci <travis@travis-ci.org>
2016-07-17 11:42:01 +0000

    Updated documentation for release v0.6.2

---
+++ v0.6.2/implementors/core/iter/traits/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["prettytable"] = ["impl&lt;A: <a class='trait' href='https://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.FromIterator.html' title='core::iter::traits::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='prettytable/row/struct.Row.html' title='prettytable::row::Row'>Row</a>","impl&lt;B: <a class='trait' href='https://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a>, A: <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a>&lt;Item=B&gt;&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.FromIterator.html' title='core::iter::traits::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='prettytable/struct.Table.html' title='prettytable::Table'>Table</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
