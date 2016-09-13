cefe503fd8664a2224978c3b017eb82cfca208fc
travis-ci <travis@travis-ci.org>
2016-09-13 19:24:51 +0000

    Updated documentation for release v0.6.4

---
+++ v0.6.4/implementors/rustc_serialize/serialize/trait.Decoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["csv"] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='csv/struct.Decoded.html' title='csv::Decoded'>Decoded</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
