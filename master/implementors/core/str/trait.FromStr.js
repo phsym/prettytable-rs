4f50d773e0eabee7b0d8e6b2defdf8f00fd488b2
travis-ci <travis@travis-ci.org>
2016-09-13 18:55:04 +0000

    Updated documentation on master for 87ed6be60186a904d5192e1fbaf7342e12bff37b

--- master/implementors/core/str/trait.FromStr.js
+++ master/implementors/core/str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];
+implementors["lazy_static"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='encode_unicode/struct.Utf8Char.html' title='encode_unicode::Utf8Char'>Utf8Char</a>",];implementors["libc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ master/implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='encode_unicode/struct.Utf8Iterator.html' title='encode_unicode::Utf8Iterator'>Utf8Iterator</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
