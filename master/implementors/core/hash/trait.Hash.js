b75e3bf69feccb340f56f4e46087f0debc41fc0f
travis-ci <travis@travis-ci.org>
2016-10-02 14:35:01 +0000

    Updated documentation on master for 16d8ffd7fbe7a5d892c026cb99306a3570d87be7

--- master/implementors/core/hash/trait.Hash.js
+++ master/implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='encode_unicode/struct.Utf8Char.html' title='encode_unicode::Utf8Char'>Utf8Char</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='encode_unicode/struct.Utf16Char.html' title='encode_unicode::Utf16Char'>Utf16Char</a>",];
+implementors["libc"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='encode_unicode/struct.Utf8Char.html' title='encode_unicode::Utf8Char'>Utf8Char</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='encode_unicode/struct.Utf16Char.html' title='encode_unicode::Utf16Char'>Utf16Char</a>",];implementors["lazy_static"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/ops/trait.DerefMut.js
+++ master/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["csv"] = ["impl&lt;R,&nbsp;I&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='csv/index/struct.Indexed.html' title='csv::index::Indexed'>Indexed</a>&lt;R,&nbsp;I&gt;",];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["csv"] = ["impl&lt;R,&nbsp;I&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='csv/index/struct.Indexed.html' title='csv::index::Indexed'>Indexed</a>&lt;R,&nbsp;I&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
