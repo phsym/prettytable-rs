d541105294aae1061b2976bd1de372edbf750a2a
travis-ci <travis@travis-ci.org>
2016-10-02 16:08:45 +0000

    Updated documentation for release v0.6.6

---
+++ v0.6.6/implementors/rustc_serialize/serialize/trait.Encoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["csv"] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encoder.html' title='rustc_serialize::serialize::Encoder'>Encoder</a> for <a class='struct' href='csv/struct.Encoded.html' title='csv::Encoded'>Encoded</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
