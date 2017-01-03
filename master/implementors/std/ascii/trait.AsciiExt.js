305277447a3d4c7f0b83f4e272aaf8d50eeb418a
travis-ci <travis@travis-ci.org>
2017-01-03 08:36:09 +0000

    Updated documentation on master for f5546db48499c7f8dd737f880f042d56a970a768

--- master/implementors/std/ascii/trait.AsciiExt.js
+++ master/implementors/std/ascii/trait.AsciiExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/ascii/trait.AsciiExt.html' title='std::ascii::AsciiExt'>AsciiExt</a> for <a class='struct' href='encode_unicode/struct.Utf8Char.html' title='encode_unicode::Utf8Char'>Utf8Char</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/ascii/trait.AsciiExt.html' title='std::ascii::AsciiExt'>AsciiExt</a> for <a class='struct' href='encode_unicode/struct.Utf16Char.html' title='encode_unicode::Utf16Char'>Utf16Char</a>",];implementors["libc"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/ascii/trait.AsciiExt.html' title='std::ascii::AsciiExt'>AsciiExt</a> for <a class='struct' href='encode_unicode/struct.Utf8Char.html' title='encode_unicode::Utf8Char'>Utf8Char</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/ascii/trait.AsciiExt.html' title='std::ascii::AsciiExt'>AsciiExt</a> for <a class='struct' href='encode_unicode/struct.Utf16Char.html' title='encode_unicode::Utf16Char'>Utf16Char</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
