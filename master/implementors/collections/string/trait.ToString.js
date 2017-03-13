051a1a75481797f5f3a7f7bfa5763ea12083479e
travis-ci <travis@travis-ci.org>
2017-03-13 10:02:55 +0000

    Updated documentation on master for 69d079fb5bc1abe8699ea024a7dcf588ca8762c7

--- master/implementors/collections/string/trait.ToString.js
+++ master/implementors/collections/string/trait.ToString.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["prettytable"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a> for <a class='struct' href='prettytable/cell/struct.Cell.html' title='prettytable::cell::Cell'>Cell</a>",];
+implementors["libc"] = [];
+implementors["prettytable"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/collections/string/trait.ToString.html' title='collections::string::ToString'>ToString</a> for <a class='struct' href='prettytable/cell/struct.Cell.html' title='prettytable::cell::Cell'>Cell</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/std/io/trait.Read.js
+++ master/implementors/std/io/trait.Read.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='encode_unicode/struct.Utf8Iterator.html' title='encode_unicode::Utf8Iterator'>Utf8Iterator</a>",];
+implementors["encode_unicode"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='encode_unicode/struct.Utf8Iterator.html' title='encode_unicode::Utf8Iterator'>Utf8Iterator</a>",];
+implementors["libc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
