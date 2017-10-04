294c08a88757184286c27b2216fa3f82fdacde6b
travis-ci <travis@travis-ci.org>
2017-10-04 22:13:07 +0000

    Updated documentation on master for f36b07a7ef677c9816053ac4d5f29fc9a9c84a88

---
+++ master/implementors/alloc/string/trait.ToString.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["libc"] = [];
+implementors["prettytable"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"prettytable/cell/struct.Cell.html\" title=\"struct prettytable::cell::Cell\">Cell</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/implementors/core/ops/arith/trait.MulAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.MulAssign.html\" title=\"trait core::ops::arith::MulAssign\">MulAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html\" title=\"struct std::time::duration::Duration\">Duration</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
