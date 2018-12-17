ec269d34fa91439da26601f738fd7c97813b6c61
travis-ci <travis@travis-ci.org>
2018-12-17 15:12:52 +0000

    Updated documentation on master for fa70ca6fdb7d0d98a04961c1a74105e4f797661d

--- master/implementors/serde/de/trait.Error.js
+++ master/implementors/serde/de/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["csv"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">SerdeError</a> for <a class=\"struct\" href=\"csv/struct.DeserializeError.html\" title=\"struct csv::DeserializeError\">DeserializeError</a>",synthetic:false,types:["csv::deserializer::DeserializeError"]},];
+implementors["csv"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"csv/struct.DeserializeError.html\" title=\"struct csv::DeserializeError\">DeserializeError</a>",synthetic:false,types:["csv::deserializer::DeserializeError"]},];
 implementors["serde"] = [];
 
             if (window.register_implementors) {
--- master/implementors/serde/ser/trait.Error.js
+++ master/implementors/serde/ser/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["csv"] = [{text:"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">SerdeError</a> for <a class=\"struct\" href=\"csv/struct.Error.html\" title=\"struct csv::Error\">Error</a>",synthetic:false,types:["csv::error::Error"]},];
+implementors["csv"] = [{text:"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"csv/struct.Error.html\" title=\"struct csv::Error\">Error</a>",synthetic:false,types:["csv::error::Error"]},];
 implementors["serde"] = [];
 
             if (window.register_implementors) {
