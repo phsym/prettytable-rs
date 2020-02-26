acbfcff6b8c18c7e31468b64891727072b67217b
travis-ci <travis@travis-ci.org>
2020-02-26 17:34:01 +0000

    Updated documentation on master for cf7dca33e2e2131c20e4bbc34ad222c5e6edc29a

--- master/implementors/core/convert/trait.Into.js
+++ master/implementors/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prettytable"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"prettytable/format/struct.TableFormat.html\" title=\"struct prettytable::format::TableFormat\">TableFormat</a>&gt; for <a class=\"struct\" href=\"prettytable/format/struct.FormatBuilder.html\" title=\"struct prettytable::format::FormatBuilder\">FormatBuilder</a>",synthetic:false,types:["prettytable::format::FormatBuilder"]},];
+implementors["prettytable"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"prettytable/format/struct.TableFormat.html\" title=\"struct prettytable::format::TableFormat\">TableFormat</a>&gt; for <a class=\"struct\" href=\"prettytable/format/struct.FormatBuilder.html\" title=\"struct prettytable::format::FormatBuilder\">FormatBuilder</a>","synthetic":false,"types":["prettytable::format::FormatBuilder"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/serde/ser/trait.Serialize.js
+++ master/implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bstr"] = [{text:"impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bstr/struct.BStr.html\" title=\"struct bstr::BStr\">BStr</a>",synthetic:false,types:["bstr::bstr::BStr"]},{text:"impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bstr/struct.BString.html\" title=\"struct bstr::BString\">BString</a>",synthetic:false,types:["bstr::bstring::BString"]},];
+implementors["bstr"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bstr/struct.BStr.html\" title=\"struct bstr::BStr\">BStr</a>","synthetic":false,"types":["bstr::bstr::BStr"]},{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bstr/struct.BString.html\" title=\"struct bstr::BString\">BString</a>","synthetic":false,"types":["bstr::bstring::BString"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
